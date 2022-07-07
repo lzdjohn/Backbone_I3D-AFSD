bc286cd0c08d85d1075665685ae3584404900485
lzdjohn <47667100+lzdjohn@users.noreply.github.com>
2022-07-07 21:12:35 +0800

    Delete AFSD directory

--- AFSD/anet_data/gen_video_list.py
+++
@@ -1,6 +0,0 @@
-import glob
-import numpy as np
-
-video_list = sorted(glob.glob('datasets/activitynet/train_val_112/*.mp4'))
-
-np.savetxt('anet_anotations/anet_train_val.txt', video_list, '%s', '\n')
